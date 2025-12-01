4a0c89866bbd6924ca3afebc749674015f043e55
iAtoo-dev <agent.iatoo@gmail.com>
2025-12-01 13:15:51 +0100

    Create oscaro_session.py

---
+++ oscaro_session.py
@@ -0,0 +1,19 @@
+# oscaro_session.py
+import requests
+
+def get_oscaro_session():
+    session = requests.Session()
+    session.headers.update({
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Language": "fr-FR,fr;q=0.9",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+    })
+    # Pré-charge la home pour cookies + cf_clearance
+    try:
+        session.get("https://www.oscaro.com/", timeout=10)
+    except:
+        pass
+    return session
